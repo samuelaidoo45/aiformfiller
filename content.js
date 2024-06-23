60d6b4d51a707f9e19f2ce81ef885a0701a97c5d
samuelaidoo45 <samuelaidoo45@gmail.com>
2024-06-23 10:04:45 -0400

    Update: form filling completed

--- content.js
+++ content.js
@@ -1,36 +1,63 @@
 function collectInputValues() {
-    const inputs = document.querySelectorAll('input, select, textarea');
-    const inputValues = {};
+    const inputs = document.querySelectorAll('input, textarea');
+    const inputValues = [];
+
 
     inputs.forEach(input => {
         let id = input.id || "";
         let name = input.name || "";
         let type = input.type || "";
         
+            let inputInfo = {};
+
+            if (input.labels.length > 0) {
+                inputInfo['label'] = input.labels[0].innerText;
+            }
+            if (input.id) {
+                inputInfo['id'] = input.id;
+            }
+            if (input.name) {
+                inputInfo['name'] = input.name;
+            }
+            if (input.placeholder) {
+                inputInfo['placeholder'] = input.placeholder;
+            }
+            if (input.type) {
+                inputInfo['type'] = input.type;
+            }
+
+            inputInfo['value'] = "";
+
+            inputValues.push(inputInfo);
        
-        // if (input.type === 'checkbox' || input.type === 'radio') {
-        //     if (input.checked) {
-        //         if (inputValues[input.name]) {
-        //             inputValues[input.name].push(input.value);
-        //         } else {
-        //             inputValues[input.name] = [input.value];
-        //         }
-        //     }
-        // } else {
-        //     inputValues[input.id] = input.value;
-        // }
     });
 
-    let selects = document.querySelectorAll('select');
-
-    let textareas = document.querySelectorAll('textarea');
-
     return inputValues;
+
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "getInputValues") {
         const values = collectInputValues();
-        sendResponse(values);
+
+        fetch('https://tivateonline.com/aiformfiller.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+    })
+    .then(response => response.json()) // parse the response body as JSON
+    .then(data => {
+        data.forEach(item => {
+            let inputField = document.getElementById(item.id);
+            if (inputField) {
+                inputField.value = item.value;
+            }
+        });
+    })
+    .catch(error => console.log('Error:', error));
+        //sendResponse(values);
     }
 });
+
